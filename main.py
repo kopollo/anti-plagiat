3413935bffcbbbb8d14015032897f8d8eb1967c6
unknown <andreysamsaa@gmail.com>
2022-09-28 21:22:48 +0300

    base functions

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QVBoxLayout,
+    QButtonGroup,
+    QCalendarWidget,
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class PlannerWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('widget.ui', self)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    planner = PlannerWidget()
+    planner.show()
+    sys.exit(app.exec())
