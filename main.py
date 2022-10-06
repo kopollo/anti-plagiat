882d1b4906a7152b22a95fd048f55af9bfe10672
unknown <andreysamsaa@gmail.com>
2022-10-06 22:45:59 +0300

    update architecture and add docstring

--- main.py
+++ main.py
@@ -3,19 +3,54 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout,
-    QButtonGroup,
-    QCalendarWidget,
+    QFileDialog, QLineEdit, QDialog, QPushButton, QWidget, QTextEdit
 )
 
 
-class PlannerWidget(QMainWindow):
+class Antiplagiat(QMainWindow):
+    """
+    Main class that describe Antiplagiat system
+    """
     def __init__(self):
+        """
+        Initialize main window of Antiplagiat, sets window title
+        """
         super().__init__()
-        uic.loadUi('widget.ui', self)
+        uic.loadUi('main_widget.ui', self)
+        self.setWindowTitle("Антиплагиат")
+
+    def click_on_history_btn(self):
+        """
+        open a new window with history of requests
+        """
+
+    def click_on_settings_btn(self):
+        """
+        open a new window with settings dialog
+        """
+
+    def click_on_compare_btn(self):
+        """
+        call a compare function after click
+        """
+
+    def click_on_save_result_btn(self):
+        """
+        saves current compare attributes
+        (equality percent, request time) to database
+        """
+
+    def levenshtein_distance(self, text1, text2):
+        """
+        finds levenshtein distance of two texts
+        :param text1: first text
+        :param text2: second text
+        :return: levenshtein distance
+        """
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    planner = PlannerWidget()
-    planner.show()
+    antiplagiat = Antiplagiat()
+    antiplagiat.show()
     sys.exit(app.exec())
---
+++ myWidgets.py
@@ -0,0 +1,41 @@
+﻿"""
+file that contains custom widgets
+"""
+from PyQt5.QtWidgets import (
+    QFileDialog, QLineEdit, QDialog, QPushButton, QWidget, QPlainTextEdit,
+    QVBoxLayout
+)
+
+
+class DisplayTextWidget(QWidget):
+    """
+    Widget that allows to add text from file and to see that text
+    """
+    def __init__(self, parent):
+        """
+        Initialize DisplayTextWidget widget
+        :param parent: allows to promote QWidget
+        """
+        super(DisplayTextWidget, self).__init__(parent=parent)
+
+        self.open_file_dialog_btn = QPushButton('Add text from file', self)
+        self.open_file_dialog_btn.clicked.connect(self.get_file)
+
+        self.text = QPlainTextEdit(self)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.open_file_dialog_btn)
+        self.layout.addWidget(self.text)
+
+    def get_file(self):
+        """
+        open text by file dialog and shows it in text widget
+        """
+        try:
+            file = QFileDialog.getOpenFileName(self, 'Open file')
+            file_path, file_type = file
+            text = open(file_path).read()
+            self.text.setPlainText(text)
+        except FileNotFoundError:
+            # maybe should save that info
+            pass
